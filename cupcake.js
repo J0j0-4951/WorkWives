bd56da7461d22f6f951d28ea13800b8ed6a199d3
NeoAtlasz <y0shii103288@gmail.com>
2025-06-25 09:23:26 -0700

    added images

---
+++ cupcake.js
@@ -0,0 +1,21 @@
+const scoops = {
+    Strawberry: document.querySelector('#scoop-strawberry'),
+    Green: document.querySelector('#scoop-Green'),
+    Chocolate: document.querySelector('#scoop-chocolate'),
+};
+const buttons = {
+    Strawberry: document.getElementById('btn-strawberry'),
+    Green: document.getElementById('btn-Green'),
+    Chocolate: document.getElementById('btn-Chocolate'),
+};
+
+const counters = {
+    Strawberry: 0,
+    Green: 0,
+    Chocolate: 0,
+};
+
+buttons.Strawberry.addEventListener('click', () => {
+    let Strawberry =+1;
+    
+});
