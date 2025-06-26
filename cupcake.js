d8f58d691b34b10830eddd2364f98a60da1d0e5c
NeoAtlasz <y0shii103288@gmail.com>
2025-06-26 09:30:49 -0700

    frosting buttons work almost

--- cupcake.js
+++ cupcake.js
@@ -25,24 +25,31 @@ const images = document.getElementById('cupcakeImage');
 
 //buttons
 
-function addImage() {
+function addPinkFrosting(color) {
+    const frostingImage = document.getElementById("pinkFrostingImage");
 
-const selectedVaule = document.getElementById('PinkFrosting').value;
-const images = document.getElementById('cupcakeImage');
+    if (color === 'PinkFrosting') {
+        frostingImage.src = 'images/PinkFrosting.PNG';
 
-   if (selectedVaule === 'EmptyLiner')
-    {cupcakeImage.src = 'images/EmptyLiner.PNG';}
-
-     else if (selectedVaule === 'MediumRareCupcake')
-
-        {cupcakeImage.src = 'images/MediumRareCupcake.PNG';}
-
-            else if (selectedVaule === 'ChocoCupcake')
-                {cupcakeImage.src = 'images/ChocoCupcake.PNG';  }
-
-            else if (selectedVaule === 'VanillaCupcake')
-
-        {cupcakeImage.src = 'images/VanillaCupcake.PNG';}
-
-
+        frostingImage.style.display = 'block';
+    }
 }
+
+    function addBlueFrosting(color) {
+    const frostingImage = document.getElementById("blueFrostingImage");
+
+    if (color === 'BlueFrosting') {
+        frostingImage.src = 'images/BlueFrosting.PNG';
+    }
+        frostingImage.style.display = 'block';
+    }
+
+
+    function addOrangeFrosting(color) {
+    const frostingImage = document.getElementById("orangeFrostingImage");
+
+    if (color === 'OrangeFrosting') {
+        frostingImage.src = 'images/OrangeFrosting.PNG';
+    }
+        frostingImage.style.display = 'block';
+    }
